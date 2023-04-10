61dd57f2b753318c5f36ea4a96e21be43adf3c60
lenayak <113600536+lenayak@users.noreply.github.com>
2023-04-10 14:20:03 +0400

    decryption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 
-import generation_keys, encryption
+import generation_keys, encryption, decryption
 from info import *
 
 
@@ -16,8 +16,8 @@ if args.generation is not None:
     private_key = keys[0]
     public_key = keys[1]
     generation_keys.serializate(private_key, public_key, private_pem, public_pem)
-    cipher = generation_keys.encrypt_key(public_key, sym_key)
-    generation_keys.key_to_file(symmetric_encrypt_key, cipher)
+    cipher_key = generation_keys.encrypt_key(public_key, sym_key)
+    generation_keys.key_to_file(symmetric_encrypt_key, cipher_key)
 elif args.encryption is not None:
     print('Encryption\n')
     private_key = encryption.get_private_key(private_pem)
@@ -26,4 +26,6 @@ elif args.encryption is not None:
     encryption.save_to_file(encrypted_file, cipher_text)
 elif args.decryption is not None:
     print('Decryption\n')
-
+    private_key = decryption.get_private_key(private_pem)
+    sym_key = decryption.decrypt_key(symmetric_encrypt_key, private_key)
+    cipher_text = decryption.decrypt_text(sym_key, encrypted_file, decrypted_file)
